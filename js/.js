488c7f0111e746f7fa3d01d688f47369970b3afd
6xxnnk <wholovesthesun@naver.com>
2026-02-06 13:44:45 +0900

    중간저장

--- js/.js
+++
@@ -1,5 +0,0 @@
-const greeting = document.querySelector('.greeting');
-const io = new IntersectionObserver(([e])=>{
-  if(e.isIntersecting) greeting.classList.add('is-visible');
-},{ threshold: 0.3 });
-io.observe(greeting);
